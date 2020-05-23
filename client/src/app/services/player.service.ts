b8290c95ee98ecf93d53adcfa55a69cd68536df6
Killian <killian.bourgeat@outlook.fr>
2020-05-23 02:43:01 +0200

    add service models

---
+++ client/src/app/services/player.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerService {
+
+  // private readonly defaultRoute = ""
+
+  // constructor(
+  //   private readonly http: HttpClient,
+  // ) { }
+
+  // public getPlayer(): Observable<PlayerM[]> {
+  //   return this.http
+  // }
+}
