2ed7d7e04d1ab4cd3cfe33149aeb3e85308b7427
Clément la_selva <clement.la-selva@etu.unice.fr>
2020-05-29 11:17:34 +0200

    quick fix

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -11,7 +11,7 @@ module.exports.createGame = async(req, res) => {
         });
     }
     const game = new Game({
-      catagories: req.body.catagories,
+      categories: req.body.categories,
       ongoing: true
     });
   
