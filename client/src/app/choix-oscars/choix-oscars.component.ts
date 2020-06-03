d7830ba6178af6f50cfecc5eda15178b55e854a7
Clément la_selva <clement.la-selva@etu.unice.fr>
2020-06-03 18:08:27 +0200

    add choix oscars

---
+++ client/src/app/choix-oscars/choix-oscars.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choix-oscars',
+  templateUrl: './choix-oscars.component.html',
+  styleUrls: ['./choix-oscars.component.css']
+})
+export class ChoixOscarsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
