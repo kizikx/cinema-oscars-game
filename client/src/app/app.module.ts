9291165542ba0cd1fe8d7c3c8cb6030eefea3fb1
Clément la_selva <clement.la-selva@etu.unice.fr>
2020-06-04 18:48:05 +0200

    add material select

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AccueilComponent } from './components/accueil/accueil.component';
 import { VoteComponent } from './components/vote/vote.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
+import {MatSelectModule} from '@angular/material/select';
 import { ChoixJoueurComponent } from './components/choix-joueur/choix-joueur.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ListeGamesComponent } from './components/liste-games/liste-games.component';
@@ -45,6 +46,7 @@ import { ChoixOscarsComponent } from './components/choix-oscars/choix-oscars.com
     MatCheckboxModule,
     MatFormFieldModule,
     MatDialogModule,
+    MatSelectModule,
     MatGridListModule,
     BrowserAnimationsModule,
     MatInputModule,
